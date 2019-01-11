3821a8d2681a8607ba40fe75e35a44e25b3680a6
Jordi <jordi@kodilab.com>
2019-01-11 22:21:00 +0100

    add translation operations

--- src/Traits/Translatable.php
+++ src/Traits/Translatable.php
@@ -17,31 +17,134 @@ trait Translatable
         return parent::__get($name);
     }
 
-    public function getTranslatedAttribute($attribute, $language =  null, $honestly = false)
+    public function __call($name, $arguments)
+    {
+        if ($this->isTranslatableAttribute($name))
+        {
+            return $this->getTranslatedAttribute($name, ...$arguments);
+        }
+
+        return parent::__call($name, $arguments);
+    }
+
+    /**
+     * Get the translated $attribute_name using the $language
+     *
+     * @param string $attribute_name
+     * @param Language $language
+     * @param bool $honestly
+     * @return mixed|string
+     * @throws Exceptions\MissingLanguageException
+     */
+    public function getTranslatedAttribute(string $attribute_name, Language $language = null, $honestly = false)
     {
         if (is_null($language))
         {
             $language = Language::getUserLanguage();
         }
 
-        $translation = DB::table($this->getTable() . '_i18n')->where('language_id', $language->id)
-            ->where('resource_id', $this->id)->first([$attribute]);
+        $translation = $this->getTranslationQuery($language)->first([$attribute_name]);
 
         if (is_null($translation) && $language->id !== Language::getFallbackLanguage() && !$honestly)
         {
-            return $this->getTranslatedAttribute($attribute, Language::getFallbackLanguage());
+            return $this->getTranslatedAttribute($attribute_name, Language::getFallbackLanguage());
         }
 
-        return !is_null($translation) ? $translation->$attribute : "";
+        return !is_null($translation) ? $translation->$attribute_name : "";
     }
 
-    private function isTranslatableAttribute(string $attribute)
+    /**
+     * Update the $attributes of a $language translation. The attributes which are not present in $attributes
+     * will not be updated. If the translation doesn't exists then it will be created.
+     *
+     * @param array $attributes
+     * @param null $language
+     * @throws Exceptions\MissingLanguageException
+     */
+    public function updateTranslation(array $attributes, Language $language = null)
+    {
+        if (is_null($language))
+        {
+            $language = Language::getFallbackLanguage();
+        }
+
+        $attributes = $this->filterValidTranslatableAttributes($attributes);
+
+        if (!$this->hasTranslation($language))
+        {
+            $this->createTranslation($attributes, $language);
+            return;
+        }
+
+        $this->getTranslationQuery($language)->update($attributes);
+    }
+
+    /**
+     * Create a translation using the values in $attributes for a given $language
+     * @param array $attributes
+     * @param Language|null $language
+     * @throws Exceptions\MissingLanguageException
+     */
+    public function createTranslation(array $attributes, Language $language = null)
+    {
+        if (is_null($language))
+        {
+            $language = Language::getFallbackLanguage();
+        }
+
+        if ($this->hasTranslation($language))
+        {
+            return;
+        }
+
+        $attributes = $this->filterValidTranslatableAttributes($attributes);
+
+        // As $attributes could not contains all translatable attributes and we can't insert only a part of them
+        // we need to fill the not present ones with as empty string
+        $attributes = $this->fillTranslatedAttributes($attributes);
+
+        $attributes['language_id'] = $language->id;
+        $attributes['resource_id'] = $this->id;
+
+        DB::table($this->getTranslationsTableName())->insert($attributes);
+    }
+
+    /**
+     * Returns whether a translation for a language exists
+     *
+     * @param $language
+     * @return bool
+     */
+    public function hasTranslation( Language $language)
+    {
+        return !is_null($this->getTranslationQuery($language)->get());
+    }
+
+    /**
+     * Return the query of the translation
+     * @param $language
+     * @return \Illuminate\Database\Query\Builder
+     */
+    private function getTranslationQuery(Language $language)
+    {
+        return DB::table($this->getTranslationsTableName())
+            ->where('language_id', $language->id)
+            ->where('resource_id', $this->id);
+    }
+
+    /**
+     * Returns whether $attribute is a translatable attribute (has a column in the translation table)
+     *
+     * @param string $attribute
+     * @return bool
+     */
+    private function isTranslatableAttribute(string $attribute_name)
     {
         $columns = $this->getTranslatableAttributes();
 
         foreach ($columns as $column)
         {
-            if ($column === $attribute)
+            if ($column === $attribute_name)
             {
                 return true;
             }
@@ -50,9 +153,57 @@ trait Translatable
         return false;
     }
 
+    /**
+     * Remove attributes from $attributes which are not translatable
+     *
+     * @param array $attributes
+     * @return array
+     */
+    private function filterValidTranslatableAttributes(array $attributes)
+    {
+        $expected = $this->getTranslatableAttributes();
+
+        foreach ($attributes as $name => $value)
+        {
+            if (!in_array($name, $expected))
+            {
+                unset($attributes[$name]);
+            }
+        }
+
+        return $attributes;
+    }
+
+    /**
+     * Fill with "" the translated attributes which are not present in $attributes
+     *
+     * @param array $attributes
+     * @return array
+     */
+    private function fillTranslatedAttributes(array $attributes)
+    {
+        $expecteds = $this->getTranslatableAttributes();
+
+        $names = array_keys($attributes);
+
+        foreach ($expecteds as $expected)
+        {
+            if (!in_array($expected, $names))
+            {
+                $attributes[$expected] = "";
+            }
+        }
+
+        return $attributes;
+    }
+
+    /**
+     * Get the columns present in the translation table which are attributes
+     * @return mixed
+     */
     private function getTranslatableAttributes()
     {
-        $columns = Schema::getColumnListing($this->getTable() . '_i18n');
+        $columns = Schema::getColumnListing($this->getTranslationsTableName());
 
         foreach ($columns as $key => $name)
         {
@@ -63,5 +214,14 @@ trait Translatable
         }
 
         return $columns;
+    }
+
+    /**
+     * Get the name of the translations table
+     * @return string
+     */
+    private function getTranslationsTableName()
+    {
+        return $this->getTable() . '_i18n';
     }
 }
