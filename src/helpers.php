cb465b6f0b1ad3f68019e01445345fe59ac151d3
Jordi <jordi@kodilab.com>
2019-01-28 23:13:47 +0100

    Add Helpers tests

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,7 @@
 
 function t(string $text, $replace = [], $locale = null, $honestly = false)
 {
-    return app(\Kodilab\LaravelI18n\I18n::class)->translate($text, $replace, $locale, $honestly);
+    return app(\Kodilab\LaravelI18n\I18n\I18n::class)->translate($text, $replace, $locale, $honestly);
 }
 
 
