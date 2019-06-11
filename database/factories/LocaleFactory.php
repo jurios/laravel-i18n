f72b5ffe6a091713325e4b34864caae044f3798f
Jordi <jordi@kodilab.com>
2019-06-11 16:21:53 +0200

    Fix tests

--- database/factories/LocaleFactory.php
+++ database/factories/LocaleFactory.php
@@ -3,7 +3,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(\Kodilab\LaravelI18n\Models\Locale::class, function (Faker $faker) {
-    $language_iso = $faker->unique()->randomLetter . $faker->unique()->randomLetter;
+
+    $language_iso = $faker->unique()->languageCode;
+
+    while(!is_null(\Kodilab\LaravelI18n\Models\Locale::where('iso', $language_iso)->first())) {
+        $language_iso = $faker->unique()->languageCode;
+    }
+
     return [
         'iso' => $language_iso,
         'description' => $faker->text,
