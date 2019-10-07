8126ed61fd3db71231e7a696af009ac0ff5c625e
Jordi <jordi@kodilab.com>
2019-10-07 13:54:54 +0200

    Fix migration

--- tests/Unit/Traits/HasTranslations/Fixtures/database/migrations/0000_00_00_000001_create_test_tables.php
+++ tests/Unit/Traits/HasTranslations/Fixtures/database/migrations/0000_00_00_000001_create_test_tables.php
@@ -22,7 +22,7 @@ class CreateTestTables extends Migration
 
         Schema::create('translatable_model_translations', function (Blueprint $table) {
             $table->unsignedBigInteger('model_id')->nullable(false)->default(null);
-            $table->foreign('model_id')->references('id')->on('{{table}}')->onDelete('cascade');
+            $table->foreign('model_id')->references('id')->on('translatable_models')->onDelete('cascade');
 
             $table->unsignedBigInteger('locale_id')->nullable(false)->default(null);
             $table->foreign('locale_id')->references('id')->on(config('i18n.tables.locales', 'locales'))->onDelete('cascade');
